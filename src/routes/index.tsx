b324e429a1f1c238377b07fb7d33bb2c3d5c3610
Guilherme Moura <glhrmoura@gmail.com>
2025-08-02 11:28:59 -0300

    feat: convert routes to TypeScript and update to React Router v6

---
+++ src/routes/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import Navbar from '../components/navbar'
+import { GithubIcon } from '../components/icons/Github'
+import Home from '../pages/home'
+
+import {
+  FloatButton,
+  NubankInnerLayout,
+} from '../theme/global-styles'
+
+const Router: React.FC = () => {
+  const redirect = (): void => {
+    window.open('https://github.com/glhrmoura/nubank-web', '_blank')
+  }
+
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <NubankInnerLayout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </NubankInnerLayout>
+      <FloatButton onClick={redirect}>
+        <GithubIcon />
+      </FloatButton>
+    </BrowserRouter>
+  )
+}
+
+export default Router 
