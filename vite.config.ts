07a27a9814f7c5927649a81ef3d3940ccf80be71
Guilherme Moura <glhrmoura@gmail.com>
2025-08-02 12:09:57 -0300

    chore: organize imports in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import react from '@vitejs/plugin-react'
 import path from 'path'
 import { fileURLToPath } from 'url'
+
+import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
