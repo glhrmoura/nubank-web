12458bbe85ada6703b587e84ea44dcd615b9e027
Guilherme Moura <glhrmoura@gmail.com>
2025-08-02 11:28:11 -0300

    feat: create new main entry point with React 18 and TypeScript

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './assets/styles/index.scss'
+import NubankWeb from './routes'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <NubankWeb />
+  </React.StrictMode>
+) 
