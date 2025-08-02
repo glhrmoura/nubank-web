7b1f06806f21e5a8d68f5b2f9b3125780d842b04
Guilherme Moura <glhrmoura@gmail.com>
2025-08-02 12:21:07 -0300

    feat: add React Router v7 future flags to prepare for upgrade

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -15,7 +15,12 @@ const Router: React.FC = () => {
   }
 
   return (
-    <BrowserRouter>
+    <BrowserRouter
+      future={{
+        v7_startTransition: true,
+        v7_relativeSplatPath: true,
+      }}
+    >
       <Navbar />
       <NubankInnerLayout>
         <Routes>
